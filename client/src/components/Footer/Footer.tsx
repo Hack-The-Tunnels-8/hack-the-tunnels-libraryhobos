9f713c2d6e119a0544d6171a144d37947e894ece
Pathmann25 <90112346+Pathmann25@users.noreply.github.com>
2023-09-30 17:00:45 +0000

    footer challenge

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,7 +1,11 @@
 import "./Footer.style.scss";
 
 function Footer() {
-  return <div className="footer">footer</div>;
+  return (
+    <div className="footer">
+      <div>© 2023 Carleton University</div>
+    </div>
+  );
 }
 
 export default Footer;
